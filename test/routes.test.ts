bc68571f36fbeda93a1882ac4443f3222cbef61d
wobsoriano <sorianorobertc@gmail.com>
2023-07-24 07:55:32 -0700

    add routes initial test

---
+++ test/routes.test.ts
@@ -0,0 +1,15 @@
+import { describe, test, expect } from "bun:test";
+import Elysia from "elysia";
+import { autoroutes } from "../src";
+
+describe('routes', () => {
+  test('basic', async () => {
+    const app = new Elysia().use(autoroutes({ routesDir: './routes' }))
+    
+    const response = await app.handle(
+        new Request('http://localhost/basic')
+    ).then(res => res.text())
+
+    expect(response).toBe('hi')
+  })
+})
