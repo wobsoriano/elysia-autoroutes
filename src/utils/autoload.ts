0d236039fe12987b1e7097e21960f292d502b4cc
wobsoriano <sorianorobertc@gmail.com>
2023-07-27 10:13:38 -0700

    chore: skip invalid methods

--- src/utils/autoload.ts
+++ src/utils/autoload.ts
@@ -2,14 +2,8 @@ import fg from 'fast-glob'
 import type Elysia from 'elysia'
 import { transformPathToUrl } from './transformPathToUrl'
 
-export type ValidMethods =
-  | 'DELETE'
-  | 'GET'
-  | 'HEAD'
-  | 'PATCH'
-  | 'POST'
-  | 'PUT'
-  | 'OPTIONS'
+const validMethods = ['DELETE', 'GET', 'HEAD', 'PATCH', 'POST', 'PUT', 'OPTIONS'] as const
+type ValidMethods = typeof validMethods[number]
 
 export async function autoload(app: Elysia, dirPath: string, routePrefix: string) {
   const files = await fg('**/*.{ts,js,mjs,cjs}', {
@@ -27,6 +21,9 @@ export async function autoload(app: Elysia, dirPath: string, routePrefix: string
     const routeModule = await import(file)
 
     for (const [method, handler] of Object.entries(routeModule)) {
+      if (!validMethods.includes(method.toUpperCase() as ValidMethods))
+        continue
+
       if (typeof handler === 'function')
         app[method as unknown as Lowercase<ValidMethods>](routeName, handler as RouteHandler)
       else
