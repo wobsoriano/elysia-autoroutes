3faf85722c2012308726866d39df61c146444363
wobsoriano <sorianorobertc@gmail.com>
2023-07-28 10:10:28 -0700

    update types

--- src/utils/autoload.ts
+++ src/utils/autoload.ts
@@ -1,20 +1,21 @@
 import path from 'node:path'
 import type Elysia from 'elysia'
+import type { LocalHandler, LocalHook } from 'elysia'
 import { transformPathToUrl } from './transformPathToUrl'
 
 const validMethods = ['DELETE', 'GET', 'HEAD', 'PATCH', 'POST', 'PUT', 'OPTIONS'] as const
 type ValidMethods = typeof validMethods[number]
 
 export async function autoload(app: Elysia, routesDir: string, routePrefix: string) {
-  type RouteHandler = Parameters<Elysia['get']>[1]
-  type RouteHooks = Parameters<Elysia['get']>[2]
-
   const router = new Bun.FileSystemRouter({
     style: 'nextjs',
     dir: getDirPath(routesDir),
   })
 
-  const routeModules: Record<string, any> = {}
+  const routeModules: Record<string, Record<Lowercase<ValidMethods>, LocalHandler<any, any> | {
+    handler: LocalHandler<any, any>
+    hooks?: LocalHook<any, any>
+  }>> = {}
   const importPromises: Promise<void>[] = []
 
   for (const [nextRouteName, file] of Object.entries(router.routes)) {
@@ -35,9 +36,9 @@ export async function autoload(app: Elysia, routesDir: string, routePrefix: stri
         const normalizedMethod = method.toUpperCase() as ValidMethods
         if (validMethods.includes(normalizedMethod)) {
           if (typeof handler === 'function')
-            app[method as unknown as Lowercase<ValidMethods>](routeName, handler as RouteHandler)
+            app[method as unknown as Lowercase<ValidMethods>](routeName, handler)
           else
-            app[method as unknown as Lowercase<ValidMethods>](routeName, (handler as { handler: RouteHandler }).handler, (handler as { hooks: RouteHooks }).hooks)
+            app[method as unknown as Lowercase<ValidMethods>](routeName, handler.handler, handler.hooks)
         }
       }
     }
