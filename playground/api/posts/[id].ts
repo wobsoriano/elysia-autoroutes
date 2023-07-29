9091a52bebdc0a8955a5ca6a205c2f48b6da21b7
wobsoriano <sorianorobertc@gmail.com>
2023-07-28 22:47:59 -0700

    fix playground types

--- playground/api/posts/[id].ts
+++ playground/api/posts/[id].ts
@@ -1,7 +1,7 @@
-import { Context } from "elysia"
+import type { Context } from 'elysia'
 
 export async function get(context: Context) {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + context.params?.id)
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params?.id}`)
   const data = await response.json()
   return data
 }
